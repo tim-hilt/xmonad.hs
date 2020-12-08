8cec993b9603bdbfd5e27b650fcad6c07615c8d4
Tim Hilt <timhilt@live.de>
2020-12-08 07:58:22 +0100

    Modified layouts

--- xmonad.hs
+++ xmonad.hs
@@ -19,19 +19,26 @@ import           XMonad.Layout.EqualSpacing
 myTerminal :: String
 myTerminal = "alacritty"
 
+myModMask :: KeyMask
 myModMask = mod4Mask
 
 myStartup :: X ()
 myStartup = do
   spawn "hsetroot -cover ~/dev/dwm/assets/background.jpg"
+  spawn "xset b off"
+
+myNormalBorderColor :: String
+myNormalBorderColor = "#222222"
+
+myFocusedBorderColor :: String
+myFocusedBorderColor = "#aa0000"
 
 myLayouts =
-  equalSpacing 60 6 0 1
-    $   Tall 1 (3 / 100) (1 / 2)
-    ||| emptyBSP
-    ||| Full
-    ||| spiral (6 / 7)
-    ||| Mirror (Tall 1 (3 / 100) (1 / 2))
+  equalSpacing 60 6 0 1 (Tall 1 (3 / 100) (1 / 2))
+    ||| equalSpacing 60 6 0 1 (emptyBSP)
+    ||| equalSpacing 60 0 0 1 (Full)
+    ||| equalSpacing 60 6 0 1 (spiral (6 / 7))
+    ||| equalSpacing 60 6 0 1 (Mirror (Tall 1 (3 / 100) (1 / 2)))
 
 myKeys =
   [ ((myModMask, xK_Return)              , spawn myTerminal)
@@ -48,10 +55,12 @@ main =
   do
       xmonad
     $                ewmhFullscreen
-    $                ewmh def { terminal          = myTerminal
-                              , modMask           = myModMask
-                              , startupHook       = myStartup
-                              , focusFollowsMouse = False
-                              , layoutHook        = myLayouts
+    $                ewmh def { terminal           = myTerminal
+                              , normalBorderColor  = myNormalBorderColor
+                              , focusedBorderColor = myFocusedBorderColor
+                              , modMask            = myModMask
+                              , startupHook        = myStartup
+                              , focusFollowsMouse  = False
+                              , layoutHook         = myLayouts
                               }
     `additionalKeys` myKeys
